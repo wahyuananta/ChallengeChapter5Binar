077304303d97aab8bb92ac3a58a86ed054903efb
wahyuananta <wahyuanantajr@gmail.com>
2022-04-18 04:07:49 +0700

    Initial commit

---
+++ app/src/main/java/com/coder/challengechapter5binar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.coder.challengechapter5binar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
