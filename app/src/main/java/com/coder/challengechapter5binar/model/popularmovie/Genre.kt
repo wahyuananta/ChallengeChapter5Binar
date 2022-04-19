194321e9fb6d2d5f6611dba66b4915d6e9580879
wahyuananta <wahyuanantajr@gmail.com>
2022-04-19 15:55:48 +0700

    Add GET api detail movie

---
+++ app/src/main/java/com/coder/challengechapter5binar/model/popularmovie/Genre.kt
@@ -0,0 +1,11 @@
+package com.coder.challengechapter5binar.model.popularmovie
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
